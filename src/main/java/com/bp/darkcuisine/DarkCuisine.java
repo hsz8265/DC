1d1dd0684af8791cddc99733da0d5074bc0e0ea0
kkkklooooo <mabo27158@gmail.com>
2025-06-24 15:45:05 +0800

    TestItem

--- src/main/java/com/bp/darkcuisine/DarkCuisine.java
+++ src/main/java/com/bp/darkcuisine/DarkCuisine.java
@@ -1,5 +1,6 @@
 package com.bp.darkcuisine;
 
+import com.bp.darkcuisine.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ public class DarkCuisine implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.regModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/com/bp/darkcuisine/item/ModItems.java
@@ -0,0 +1,18 @@
+package com.bp.darkcuisine.item;
+import com.bp.darkcuisine.DarkCuisine;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public final Item tst = regItems("tst",new Item(new Item.Settings()));
+    private static Item regItems(String id,Item item){
+        //return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(,id)),item);
+        return Registry.register(Registries.ITEM, Identifier.of(DarkCuisine.MOD_ID,id),item);
+    }
+    public static void regModItems(){
+        DarkCuisine.LOGGER.info("regI");
+    }
+}
