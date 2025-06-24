7de9fc810ff669d4fa72142bf1e26a8a9dd4a55e
kkkklooooo <mabo27158@gmail.com>
2025-06-24 13:49:03 +0800

    First

---
+++ src/main/java/com/bp/darkcuisine/DarkCuisineDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bp.darkcuisine;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DarkCuisineDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
