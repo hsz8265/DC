5af047fe79ba9b72f5ba75836a68db08607f5942
hsz <hushizhih@gmail.com>
2025-06-25 19:17:42 +0800

    bug reason:manner has been deprecated

--- src/main/java/com/bp/darkcuisine/entity/client/EntityClient.java
+++ src/main/java/com/bp/darkcuisine/entity/client/EntityClient.java
@@ -3,9 +3,12 @@ package com.bp.darkcuisine.entity.client;
 import com.bp.darkcuisine.entity.EntityTesting;
 import com.bp.darkcuisine.entity.MobEntities;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import com.bp.darkcuisine.entity.custom.tsteEntity;
+@Environment(EnvType.CLIENT)
 public class EntityClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
