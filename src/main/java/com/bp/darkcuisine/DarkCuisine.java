230e1b0a222e9b13b296d12b58bb9b7a1e60e7ea
hsz <hushizhih@gmail.com>
2025-06-28 00:25:30 +0800

    fix lastest bug

--- src/main/java/com/bp/darkcuisine/DarkCuisine.java
+++ src/main/java/com/bp/darkcuisine/DarkCuisine.java
@@ -8,9 +8,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.SpawnLocationTypes;
-import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.*;
 import net.minecraft.world.Heightmap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,7 +22,7 @@ public class DarkCuisine implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	public  final EntityType<tsteEntity> mosquitoCopy=MobEntities.mosquito;
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -37,8 +35,7 @@ public class DarkCuisine implements ModInitializer {
 		FabricDefaultAttributeRegistry.register(mosquito, tsteEntity.createAttributes());
 		//???为什么这行代码会让下方mosquito总是null啊
 
-		try {
-			SpawnRestriction.register(mosquito, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING, tsteEntity::canMobSpawn);
+			SpawnRestriction.register(mosquitoCopy, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING, tsteEntity::canMobSpawn);
 
 			BiomeModifications.addSpawn(biomeSelectionContext -> {
 				if (0.45f <= biomeSelectionContext.getBiome().getTemperature() && biomeSelectionContext.getBiome().getTemperature() <= 0.75f) {
@@ -46,25 +43,21 @@ public class DarkCuisine implements ModInitializer {
 				} else {
 					return false;
 				}
-			}, SpawnGroup.CREATURE, mosquito, 50, 1, 3);
+			}, SpawnGroup.CREATURE, mosquitoCopy, 15, 1, 2);
 			BiomeModifications.addSpawn(biomeSelectionContext -> {
 				if (0.75f <= biomeSelectionContext.getBiome().getTemperature() && biomeSelectionContext.getBiome().getTemperature() <= 1.2f) {
 					return true;
 				} else {
 					return false;
 				}
-			}, SpawnGroup.CREATURE, mosquito, 70, 2, 4);
+			}, SpawnGroup.CREATURE, mosquitoCopy, 25, 1, 3);
 			BiomeModifications.addSpawn(biomeSelectionContext -> {
 				if (1.2f <= biomeSelectionContext.getBiome().getTemperature()) {
 					return true;
 				} else {
 					return false;
 				}
-			}, SpawnGroup.CREATURE, mosquito, 80, 3, 8);
-		}
-		catch (Exception e)
-		{
-			LOGGER.info(String.valueOf(e));
-		}
+			}, SpawnGroup.CREATURE, mosquitoCopy, 40, 2, 3);
+
 	}
 }
--- src/main/java/com/bp/darkcuisine/entity/client/mosquitoAnimations.java
+++ src/main/java/com/bp/darkcuisine/entity/client/mosquitoAnimations.java
@@ -8,29 +8,29 @@ import net.minecraft.client.render.entity.animation.*;
  * @author Author
  */
 public class mosquitoAnimations {
-	public static final AnimationDefinition fly = AnimationDefinition.Builder.create(1.0F).looping()
+	public static final AnimationDefinition fly = AnimationDefinition.Builder.create(0.8F).looping()
 		.addBoneAnimation("wingl", new Transformation(Transformation.Targets.ROTATE,
 			new Keyframe(0.0F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(0.25F, AnimationHelper.createRotationalVector(0.0F, 0.0F, -20.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(0.75F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 20.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(1F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR)
+			new Keyframe(0.2F, AnimationHelper.createRotationalVector(0.0F, 0.0F, -20.0F), Transformation.Interpolations.LINEAR),
+			new Keyframe(0.6F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 20.0F), Transformation.Interpolations.LINEAR),
+			new Keyframe(0.8F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR)
 		))
 		.addBoneAnimation("wingr", new Transformation(Transformation.Targets.ROTATE, 
 			new Keyframe(0.0F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(0.25F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 20.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(0.75F, AnimationHelper.createRotationalVector(0.0F, 0.0F, -20.0F), Transformation.Interpolations.LINEAR),
-				new Keyframe(1F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR)
+			new Keyframe(0.2F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 20.0F), Transformation.Interpolations.LINEAR),
+			new Keyframe(0.6F, AnimationHelper.createRotationalVector(0.0F, 0.0F, -20.0F), Transformation.Interpolations.LINEAR),
+				new Keyframe(0.8F, AnimationHelper.createRotationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR)
 		))
 		.build();
 
 	public static final AnimationDefinition attack = AnimationDefinition.Builder.create(1.0F)
 		.addBoneAnimation("bone", new Transformation(Transformation.Targets.MOVE_ORIGIN,
 			new Keyframe(0.0F, AnimationHelper.createTranslationalVector(0.0F, 0.0F, 0.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(2.0F, AnimationHelper.createTranslationalVector(0.0F, -1.0F, -1.0F), Transformation.Interpolations.LINEAR)
+			new Keyframe(1.0F, AnimationHelper.createTranslationalVector(0.0F, -1.0F, -1.0F), Transformation.Interpolations.LINEAR)
 		))
 		.addBoneAnimation("bone", new Transformation(Transformation.Targets.SCALE, 
 			new Keyframe(0.0F, AnimationHelper.createScalingVector(1.0F, 1.0F, 1.0F), Transformation.Interpolations.LINEAR),
-			new Keyframe(2.0F, AnimationHelper.createScalingVector(1.2F, 1.2F, 1.2F), Transformation.Interpolations.LINEAR)
+			new Keyframe(1.0F, AnimationHelper.createScalingVector(1.2F, 1.2F, 1.2F), Transformation.Interpolations.LINEAR)
 		))
 		.build();
 }
