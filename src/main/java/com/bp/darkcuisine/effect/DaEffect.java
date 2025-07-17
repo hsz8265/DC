857a2fef210564ca9c58ea2f47313d145588b16d
kkkklooooo <mabo27158@gmail.com>
2025-07-17 17:21:19 +0800

    Dai Effect

--- src/main/java/com/bp/darkcuisine/effect/DaEffect.java
+++ src/main/java/com/bp/darkcuisine/effect/DaEffect.java
@@ -1,8 +1,14 @@
 package com.bp.darkcuisine.effect;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
 
 public class DaEffect extends StatusEffect {
 
@@ -14,4 +20,13 @@ public class DaEffect extends StatusEffect {
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
     }
+
+    @Override
+    public boolean applyUpdateEffect(ServerWorld world, LivingEntity entity, int amplifier) {
+        entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,2,3));
+
+        return super.applyUpdateEffect(world, entity, amplifier);
+    }
+
+
 }
