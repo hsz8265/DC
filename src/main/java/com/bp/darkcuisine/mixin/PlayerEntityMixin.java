55e69755d03a124c98c86d30f6969580680ff047
kkkklooooo <mabo27158@gmail.com>
2025-07-17 09:43:07 +0800

    饱腹但有buff时仍可吃食物

---
+++ src/main/java/com/bp/darkcuisine/mixin/PlayerEntityMixin.java
@@ -0,0 +1,25 @@
+package com.bp.darkcuisine.mixin;
+
+
+import com.bp.darkcuisine.DarkCuisine;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.lang.reflect.Method;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @Inject(at = @At("HEAD"),
+            method = "canConsume", cancellable = true)
+    private void CanConsume(boolean ignoreHunger, CallbackInfoReturnable<Boolean> cir){
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        if (player.hasStatusEffect(DarkCuisine.wei)) {
+            DarkCuisine.LOGGER.info("Can!!!!!!!!!!!");
+            cir.setReturnValue(true);
+        }
+    }
+}
